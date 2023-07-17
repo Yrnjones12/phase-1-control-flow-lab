8c19da2638f35f02cd8cee3f21e651e9fe59b25d
38jones <Yrnjones12>
2023-07-17 15:23:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+  // gives customers a free sample if the ride is to 200 feet'
+
+if(scuberGreetingForFeet=== '200 feet'){
+
+return 'this one is one me!'
+
+
+} else if (scuberGreetingForFeet300==='String'){
+  return 'charge extra 100!'
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+  // ternaryCheckCity(100)
+  let result
+  if (someNumber>100){
+    result= someNumber + ternaryCheckCity;
+  }
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  // switchOnCharmFromTip(pay 400 for the trip)
+case '400 per trip';
+
+return  'pay 400 for the trip!'
+
+default ;
+
+return 'switchOnCharmFromTip ${400)!'
+
+}
+
 }
